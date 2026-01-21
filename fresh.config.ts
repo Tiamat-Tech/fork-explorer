8c5ca980ceceaac32f4294aecb77b72afc4cd072
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2026-01-22 00:42:53 +0100

    Read server and port from config

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,3 +1,9 @@
 import { defineConfig } from "$fresh/server.ts";
+import config from "./lib/config.ts";
 
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: config.server.port,
+    hostname: config.server.host,
+  },
+});
